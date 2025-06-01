c05b5cb708b6365a14e84f59ee05bee859ad1d74
Marcel Dejean <marcel@dejean.nyc>
2025-06-01 14:27:41 -0400

    automatically retry with a direction for doglegs and gaps

--- streetstretch.c
+++ streetstretch.c
@@ -52,6 +52,36 @@ struct coord intersection_coord(char node[7]) {
     return ret;
 }
 
+char function_3s(C_WA1* wa1, C_WA2_F3S* wa2, char boro, const char* on_street, const char* from_street, const char* to_street, char from_dir, char to_dir) {
+    memset(wa1, 0, sizeof(*wa1));
+    memset(wa2, 0, sizeof(*wa2));
+    wa1->input.func_code[0] = '3';
+    wa1->input.func_code[1] = 'S';
+    wa1->input.platform_ind = 'P';
+    
+    wa1->input.sti[0].boro = boro;
+    memcpy(wa1->input.sti[0].Street_name, 
+           on_street, 
+           min(sizeof(wa1->input.sti[0].Street_name), strlen(on_street)) );
+    if (from_street && to_street) {
+        wa1->input.sti[1].boro = boro;
+        memcpy(wa1->input.sti[1].Street_name, 
+               from_street, 
+               min(sizeof(wa1->input.sti[1].Street_name), strlen(from_street)) );
+        wa1->input.comp_direction = from_dir;
+        
+        wa1->input.sti[2].boro = boro;
+        memcpy(wa1->input.sti[2].Street_name, 
+               to_street, 
+               min(sizeof(wa1->input.sti[2].Street_name), strlen(to_street)) );
+        wa1->input.comp_direction2 = to_dir;
+    }
+    
+    NYCgeo((char*)wa1, (char*)wa2);
+    
+    return wa1->output.ret_code[0];
+}
+
 int main(int argc, char** argv) {
     C_WA1 wa1 = {};
     C_WA2_F3S wa2 = {};
@@ -83,35 +113,61 @@ int main(int argc, char** argv) {
     }
 #endif
     
-    wa1.input.func_code[0] = '3';
-    wa1.input.func_code[1] = 'S';
-    
     char boro = argv[1][0];
+    const char* on_street = argv[2];
     
-    wa1.input.sti[0].boro = boro;
-    memcpy(wa1.input.sti[0].Street_name, 
-           argv[2], 
-           min(sizeof(wa1.input.sti[0].Street_name), strlen(argv[2])) );
-    if (argc > 3) {
-        wa1.input.sti[1].boro = boro;
-        memcpy(wa1.input.sti[1].Street_name, 
-               argv[3], 
-               min(sizeof(wa1.input.sti[1].Street_name), strlen(argv[3])) );
+    function_3s(&wa1, &wa2, boro, on_street, argc > 4 ? argv[3] : NULL, argc > 4 ? argv[4] : NULL, 0, 0);
+    
+    // automatically retry with directions if the section is a dogleg or gap
+    if (memcmp(wa1.output.ret_code, "68", 2) == 0) {
+        // on street remains the same
+        char dirs[2] = {};
         
-        wa1.input.sti[2].boro = boro;
-        memcpy(wa1.input.sti[2].Street_name, 
-               argv[4], 
-               min(sizeof(wa1.input.sti[2].Street_name), strlen(argv[4])) );
+        for (int from_to = 0; from_to < 2; from_to++) {
+            if (argc > 5+from_to) {
+                // don't clobber user provided dirs
+                dirs[from_to] = argv[5+from_to][0];
+                continue;
+            }
+
+            // for each of the from,to streets, get the segment from the N to E intersection
+            // try N & E first
+            
+            function_3s(&wa1, &wa2, boro, on_street, argv[3+from_to], argv[3+from_to], 'N', 'E');
+            // if 'E is an invalid compass direction' then N & S
+            // if 'N is an invalid' or 'input intersections are identical' then W & E
+            if (memcmp(wa1.output.ret_code, "38", 2) == 0) {
+                if (wa1.output.msg[0] == 'E') {
+                    function_3s(&wa1, &wa2, boro, on_street, argv[3+from_to], argv[3+from_to], 'N', 'S');
+                } else if (wa1.output.msg[0] == 'N') {
+                    function_3s(&wa1, &wa2, boro, on_street, argv[3+from_to], argv[3+from_to], 'W', 'E');
+                }
+            } else if (memcmp(wa1.output.ret_code, "14", 2) == 0) {
+                function_3s(&wa1, &wa2, boro, on_street, argv[3+from_to], argv[3+from_to], 'W', 'E');
+            }
+            
+            // if there was only one intersection for this street (the other one caused the error)
+            // then we'll get 'input intersections are identical' again
+            if ((memcmp(wa1.output.ret_code, "00", 2) == 0) ||
+                (memcmp(wa1.output.ret_code, "01", 2) == 0)) {
+                size_t cross_strs_len 
+                    = (wa2.nbr_x_str[0] - '0') * 100 
+                    + (wa2.nbr_x_str[1] - '0') * 10 
+                    + (wa2.nbr_x_str[2] - '0');
+                if (cross_strs_len <= 2 && wa2.cross_strs[0].gap_flag != 'N') {
+                    // if only one segment and has gap flag = gap or dogleg
+                    // use identified direction (1 since it doesn't matter)
+                    dirs[from_to] = wa1.input.comp_direction;
+                }
+            }
+        }
         
-        if (argc > 5) {
-            wa1.input.comp_direction = argv[5][0];
-        }
-        if (argc > 6) {
-            wa1.input.comp_direction2 = argv[6][0];
-        }
+        // repeat original call with the identified directions
+        printf("no! %c %c", dirs[0], dirs[1]);
+        
+        function_3s(&wa1, &wa2, boro, on_street, argc > 4 ? argv[3] : NULL, argc > 4 ? argv[4] : NULL, dirs[0], dirs[1]);
     }
-    
-    NYCgeo((char*)&wa1, (char*)&wa2);
+
     
     if ((memcmp(wa1.output.ret_code, "00", 2) == 0) ||
         (memcmp(wa1.output.ret_code, "01", 2) == 0)) {
